c574ea055b55e9d1611bec041da84f83654d4e0c
MASANORI SONE <>
2021-04-10 22:29:56 +0900

    デザイン調整

--- pages/index.js
+++ pages/index.js
@@ -26,34 +26,39 @@ export default function Home() {
   return (
     <div>
       <Layout header="問題集" title="ITパスポート試験　令和3年春">
-      <div className="alert alert-primary text-center">
-        <h5 className="mb-4">
-          {data != undefined ? data.message : 'error...' }
-        </h5>
-            <div class="row">
-              <div  class="col-sm-12">
-                <p>問題 {data.Items[currentQuestion].no}{data.Items[currentQuestion].question[0].text}</p>
-              </div>
+        <div className="container bg-white p-4 padding text-center">
+          <h5 className="mb-4">
+            {data != undefined ? data.message : 'error...' }
+          </h5>
+          <div class="row">
+            <div  class="col-sm-12">
+              <p>問題 {data.Items[currentQuestion].no}{data.Items[currentQuestion].question[0].text}</p>
             </div>
-            {data.Items[currentQuestion].question.filter((q) => q.id!=0).map((q, key)=> (
-              <div class="row">
-                <div class="col-sm-1  text-center">
-                  <input type="radio" name={"ans_" + q.id} value={"ans_" + q.id}></input>
-                </div>
-                <div class="col-sm-1  text-center">{q.id}</div>
-                <div class="col-sm-10 text-left">{q.text}</div>
+          </div>
+          {data.Items[currentQuestion].question.filter((q) => q.id!=0).map((q, key)=> (
+            <div class="row">
+              <div class="col-sm-1  text-center">
+                <input type="radio" name={"ans_" + q.id} value={"ans_" + q.id}></input>
               </div>
-              ))}
-              <div class="row">
-                <div class="col-sm-12  text-left"></div>
-              </div>
-              <div class="row">
-                <div class="col-sm-2  text-left">答え</div>
-                <div class="col-sm-10 text-left">{data.Items[currentQuestion].answer}</div>
-              </div>
-        <button onClick={() => handleBackButtonClick()}>戻る</button>
-        <button onClick={() => handleNextButtonClick()}>次へ</button>
-      </div>
+              <div class="col-sm-1  text-center">{q.id}</div>
+              <div class="col-sm-10 text-left">{q.text}</div>
+            </div>
+          ))}
+          <div class="row">
+            <div class="col-sm-12  text-left"></div>
+          </div>
+          <div class="row">
+            <div class="col-sm-2  text-left">答え</div>
+            <div class="col-sm-10 text-left">{data.Items[currentQuestion].answer}</div>
+          </div>
+          <div class="row">
+            <div    class="col-sm-2"></div>
+            <button class="col-sm-3 btn btn-primary" onClick={() => handleBackButtonClick()}>戻る</button>
+            <div    class="col-sm-2"></div>
+            <button class="col-sm-3 btn btn-primary" onClick={() => handleNextButtonClick()}>次へ</button>
+            <div    class="col-sm-2"></div>
+          </div>
+        </div>
       </Layout>
     </div>
   )
