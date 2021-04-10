1b55f6c4cc9d8fe34350969ac96ae92a0cf9c2df
MASANORI SONE <>
2021-04-10 19:36:48 +0900

    問題の移動とBootstrapグリッドの導入

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,28 @@
 import Layout from '../components/layout'
 import useSWR from 'swr'
+import React, { useState } from 'react';
 
 export default function Home() {
   const { data } = useSWR('https://dn0fypjodt4sv.cloudfront.net/v1/scan-questions')
+  
+	const [currentQuestion, setCurrentQuestion] = useState(0)
+	const [score, setScore] = useState(0)
 
+	const handleBackButtonClick = () => {
+		const nextQuestion = currentQuestion <= 0 ? 0 : currentQuestion - 1;
+    console.log(nextQuestion)
+    setCurrentQuestion(nextQuestion)	
+	}
+
+	const handleNextButtonClick = () => {
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < data.Items.length) {
+			setCurrentQuestion(nextQuestion)		
+		} else {
+			alert('全ての問題が終了しました。');
+		}
+	}
+  
   return (
     <div>
       <Layout header="問題集" title="ITパスポート試験　令和3年春">
@@ -11,20 +30,29 @@ export default function Home() {
         <h5 className="mb-4">
           {data != undefined ? data.message : 'error...' }
         </h5>
-        <table className="table table-dark">
-          <tbody>
-            {data != undefined ? data.Items.map((value, key)=> (
-              <tr key={key}>
-                <th>問題 {value.no}</th>
-                <td>
-                  {value.question.map((value, key)=> (
-                    <p>{value.text}</p>))}
-                </td>
-                <td>{value.answer}</td>
-              </tr>
-            )) : <tr><th></th><td>no data.</td><td></td></tr>}
-          </tbody>
-        </table>
+            <div class="row">
+              <div  class="col-sm-12">
+                <p>問題 {data.Items[currentQuestion].no}{data.Items[currentQuestion].question[0].text}</p>
+              </div>
+            </div>
+            {data.Items[currentQuestion].question.filter((q) => q.id!=0).map((q, key)=> (
+              <div class="row">
+                <div class="col-sm-1  text-center">
+                  <input type="radio" name={"ans_" + q.id} value={"ans_" + q.id}></input>
+                </div>
+                <div class="col-sm-1  text-center">{q.id}</div>
+                <div class="col-sm-10 text-left">{q.text}</div>
+              </div>
+              ))}
+              <div class="row">
+                <div class="col-sm-12  text-left"></div>
+              </div>
+              <div class="row">
+                <div class="col-sm-2  text-left">答え</div>
+                <div class="col-sm-10 text-left">{data.Items[currentQuestion].answer}</div>
+              </div>
+        <button onClick={() => handleBackButtonClick()}>戻る</button>
+        <button onClick={() => handleNextButtonClick()}>次へ</button>
       </div>
       </Layout>
     </div>
